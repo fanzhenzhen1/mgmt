08de63c5312a6343983adc0869de22d9f2290892
fzz <fzz@DESKTOP-CGC1099>
2017-06-26 12:56:37 +0800

    1

---
+++ src/main/java/com/ciecc/nm/service/impl/PasswordTokenManager.java
@@ -0,0 +1,19 @@
+package com.ciecc.nm.service.impl;
+
+import com.ciecc.nm.model.User;
+
+
+public interface PasswordTokenManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    String generateRecoveryToken(User user);
+
+    /**
+     * {@inheritDoc}
+     */
+    boolean isRecoveryTokenValid(User user, String token);
+
+    void invalidateRecoveryToken(User user, String token);
+}
